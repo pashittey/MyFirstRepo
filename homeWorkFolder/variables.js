f64e857d8e2ba1a0d34876f7bb9552b580808df1
Peter <pashittey@gmail.com>
2019-07-25 19:41:28 -0500

    variables js stuff

---
+++ homeWorkFolder/variables.js
@@ -0,0 +1,43 @@
+var firstString = "";
+var secondString = "i'm not that old i'm only 2";
+3 var middleName = "nathaniel T grove";
+var parrottName = "someName";
+var dogname = "Puppy G";
+var paragraph = "this is a hard to read paragraph";
+// above are the examples of a String.
+// Strings can represent words, paragraphs, or
+// anything that needs to be text
+
+var num 1, num 2, num 3
+above show how to creat multiple variable at once
+very useful when trying to keep lines used in Code 
+at minimum.below shows how to use those variables.
+num1 = 1000;
+num2 = 100000
+num3 = 100000000
+
+var name, carCapacity, region;
+name = 100;
+carCarpacity = "six people";
+region = name + carCapacity;
+//variables dont have to be created for the name
+//creat variables to hold data
+var smallDedcimal = 10.46;
+var anotherDecimal = 1.3;
+
+var decimalThree = 33.02;
+var twoD = anotherDecimal * decimalThree;
+threeDee = twoD + anotherDecimal;
+//above represents the data type decimal in
+//use with variables
+var notTrue = true;
+var soTrue = false;
+var theNextone = notTrue;
+var anotherBoolean = true
+//booleans are True - False values. They are very useful
+//inside of conditions. in order for you to be able to
+//enhance your code and make it a little more responsive
+//you need to add conditionals. True / False conditions
+//that start or stop your code.
+
+
