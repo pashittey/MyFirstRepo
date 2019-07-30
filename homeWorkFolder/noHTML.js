724b862b51bf090dda0a39884c2ebea9be31d4b0
Peter <pashittey@gmail.com>
2019-07-29 20:55:05 -0500

    all files as of 7/29/19

---
+++ homeWorkFolder/noHTML.js
@@ -0,0 +1,31 @@
+var addingNumbers=23+45;
+console.log(addingNumbers);
+
+var sketch=true
+console.log(sketch);
+console.log(89999999*2/3666%24)
+console.log("no worries be happy")
+
+sketch={
+"Name":"Peter CottonTail",
+"Age":"23"
+}
+
+
+var x="word"
+console.log(typeof x)
+
+x=700;
+console.log(typeof x)
+
+var convertNum=1;
+console.log(convertNum);
+console.log(string(convertNum))
+
+var boo1=true;
+var scholl=false
+console.log(string(boo1))
+console.log(string(scholl))
+
+var a=7.0;
+console.log(string(a));
