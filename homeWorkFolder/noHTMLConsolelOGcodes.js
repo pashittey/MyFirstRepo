6f61ea2ab4db2ac73aeb2fc87495486d1c2ebd96
Peter <pashittey@gmail.com>
2019-07-31 20:57:46 -0500

    console.log codes

---
+++ homeWorkFolder/noHTMLConsolelOGcodes.js
@@ -0,0 +1,73 @@
+var addingNumbers=23+45;
+console.log(addingNumbers);
+
+var sketch=true;
+console.log(sketch);
+console.log(89999999*2/3666%24);
+console.log("no worries be happy");
+
+sketch={
+"Name":"Peter CottonTail",
+"Age":"23"
+}
+
+var x="word";
+
+console.log(typeof x);
+
+var y=123;
+
+console.log(typeof y);
+
+var j=true;
+
+console.log(typeof j);
+
+console.log(j+" "+x)
+
+var Profile={
+    "Name":"John Doe",
+    "Job":"Software Developer",
+    "Home":"999 innovation Dr., Lee Summit, MO",
+    "Class":"javaScript 2019"}
+
+console.log(Profile.Name);
+console.log(Profile.Job);
+console.log(Profile.Home);
+console.log(Profile.Class);
+
+console.log(Profile)
+
+
+x=700;
+console.log(typeof x);
+
+var convertNum=1;
+console.log(convertNum);
+console.log(String(convertNum));
+
+var boo1=true;
+var scholl=false;
+console.log(String(boo1));
+console.log(String(scholl));
+
+var a=7.0;
+console.log(String(a));
+
+//Number Functions
+var Five=100;
+var Seven=14;
+
+console.log(Seven.toExponential(2))
+console.log("you said"+" "+Seven.toExponential(2))
+
+var Payroll=4.56787;
+console.log(Payroll.toFixed(3));
+
+console.log(Payroll.toPrecision(3));
+
+console.log(typeof Payroll.toString());
+
+console.log(Payroll.valueOf());
+
+console.log(9%2)
